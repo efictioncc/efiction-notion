2b39f4642d6231cff7cb8603ef671493a3293bd6
에픽션 efiction <efiction.cc@gmail.com>
2024-09-20 16:56:19 +0900

    Create RollingBanner-styles.cs

---
+++ websites/Hwahae-Birdview/HwahaeBusiness/RollingBanners/RollingBanner-styles.cs
@@ -0,0 +1,12 @@
+@keyframes slideIn {from {opacity: 0; transform: translateX(5%);} to {opacity: 1; transform: translateX(0);}}
+@keyframes slideOut {from {opacity: 1; transform: translateX(0);} to {opacity: 0; transform: translateX(-5%);}}
+
+.mjr_content_area {
+  background: url(https://birdview.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fc1a845cf-b72e-4c9d-8a8f-9ad58477beec%2F2f8f3c5d-f357-4228-a96b-dcb117aad204%2Fbanner-bg-pc.webp?table=block&id=48a0d3d1-5e6b-4595-b03d-1d7df14d0e49&spaceId=c1a845cf-b72e-4c9d-8a8f-9ad58477beec&width=2000&userId=&cache=v2) no-repeat center;
+}
+
+@media (max-width: 1024px) {
+  .mjr_content_area {
+    background: url(https://birdview.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fc1a845cf-b72e-4c9d-8a8f-9ad58477beec%2F23600d1e-bccf-408d-8094-e6aa882a2bbc%2Fbanner-bg-mobile01.webp?table=block&id=294c45cb-b4ff-4e8c-b58d-59145c1bf980&spaceId=c1a845cf-b72e-4c9d-8a8f-9ad58477beec&width=1420&userId=&cache=v2) no-repeat 50% 0%;
+  }
+}
