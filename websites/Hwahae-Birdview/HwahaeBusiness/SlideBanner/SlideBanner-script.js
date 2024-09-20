4b10954f781c8d23676bfb975a35271cd2e1651d
에픽션 efiction <efiction.cc@gmail.com>
2024-09-21 00:10:25 +0900

    Update SlideBanner-script.js

--- websites/Hwahae-Birdview/HwahaeBusiness/SlideBanner/SlideBanner-script.js
+++ websites/Hwahae-Birdview/HwahaeBusiness/SlideBanner/SlideBanner-script.js
@@ -1,1 +1,20 @@
-nn
+function setSlideVisibility(slides, visibilityMap) {
+    slides.forEach((slide, index) => {
+        slide.style.display = visibilityMap[index] ? 'block' : 'none';
+    });
+}
+
+function toggleSlides() {
+    const slides = document.querySelectorAll('.oopy-slide');
+    const desktopVisibility = [true, false, true, true, true, false];
+    const mobileVisibility = [false, true, true, true, false, true];
+
+    if (window.innerWidth >= 768) {
+        setSlideVisibility(slides, desktopVisibility);
+    } else {
+        setSlideVisibility(slides, mobileVisibility);
+    }
+}
+
+window.addEventListener('load', toggleSlides);
+window.addEventListener('resize', toggleSlides);
