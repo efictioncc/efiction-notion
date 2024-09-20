8f2315cdf4a14014695edf48dbc015f86dcf047e
에픽션 efiction <efiction.cc@gmail.com>
2024-09-21 00:21:33 +0900

    Update SlideBanner-script.js

--- websites/Hwahae-Birdview/HwahaeBusiness/SlideBanner/SlideBanner-script.js
+++ websites/Hwahae-Birdview/HwahaeBusiness/SlideBanner/SlideBanner-script.js
@@ -1,20 +1,20 @@
-function setSlideVisibility(slides, visibilityMap) {
-    slides.forEach((slide, index) => {
-        slide.style.display = visibilityMap[index] ? 'block' : 'none';
-    });
-}
-
 function toggleSlides() {
-    const slides = document.querySelectorAll('.oopy-slide');
-    const desktopVisibility = [true, false, true, true, true, false];
-    const mobileVisibility = [false, true, true, true, false, true];
-
+    var slides = document.querySelectorAll('.oopy-slide');
     if (window.innerWidth >= 768) {
-        setSlideVisibility(slides, desktopVisibility);
+        slides[0].style.display = 'block';
+        slides[1].style.display = 'none';
+        slides[2].style.display = 'block';
+        slides[3].style.display = 'block';
+        slides[4].style.display = 'block';
+        slides[5].style.display = 'none';
     } else {
-        setSlideVisibility(slides, mobileVisibility);
+        slides[0].style.display = 'none';
+        slides[1].style.display = 'block';
+        slides[2].style.display = 'block';
+        slides[3].style.display = 'block';
+        slides[4].style.display = 'none';
+        slides[5].style.display = 'block';
     }
 }
-
 window.addEventListener('load', toggleSlides);
 window.addEventListener('resize', toggleSlides);
