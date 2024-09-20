4f34a6d46ff7d5b244060ecde16fdf0cf14b05cf
에픽션 efiction <efiction.cc@gmail.com>
2024-09-20 19:28:46 +0900

    Update RollingBanner-script.js

--- websites/Hwahae-Birdview/HwahaeBusiness/RollingBanners/RollingBanner-script.js
+++ websites/Hwahae-Birdview/HwahaeBusiness/RollingBanners/RollingBanner-script.js
@@ -1,25 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const ctaWrappers = document.querySelectorAll('.cta-wrapper');
-  let currentIndex = 0;
-
-  function applyAnimation(element, animationName) {
-    element.style.animation = 'none';
-    void element.offsetWidth;
-    element.style.animation = `${animationName} 0.5s ease forwards`;
-  }
-
-  function slide() {
-    const currentElement = ctaWrappers[currentIndex];
-    currentElement.classList.remove('active');
-    currentIndex = (currentIndex + 1) % ctaWrappers.length;
-    const nextElement = ctaWrappers[currentIndex];
-    nextElement.classList.add('active');
-    applyAnimation(nextElement, 'slideIn');
-    setTimeout(() => {
-      applyAnimation(nextElement, 'slideOut');
-    }, 6500);
-  }
-
-  slide();
-  setInterval(slide, 7000);
-});
+    const ctaWrappers = document.querySelectorAll('.cta-wrapper'); let currentIndex = 0;
+    function applyAnimation(element, animationName) {
+      element.style.animation = 'none'; void element.offsetWidth;
+      element.style.animation = `${animationName} 0.5s ease forwards`;
+    }
+    function slide() {
+      const currentElement = ctaWrappers[currentIndex];
+      currentElement.classList.remove('active');
+      currentIndex = (currentIndex + 1) % ctaWrappers.length;
+      const nextElement = ctaWrappers[currentIndex];
+      nextElement.classList.add('active');
+      applyAnimation(nextElement, 'slideIn');
+      setTimeout(() => { applyAnimation(nextElement, 'slideOut'); }, 6500);
+    }
+    slide();
+    setInterval(slide, 7000);
